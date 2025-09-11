07844745f3739e3ff79fcd18843e20aeea4e2cee
munoa <45395999+munoa@users.noreply.github.com>
2025-09-12 01:10:25 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css'; // adapte/retire si besoin
 
-export default function App({ Component, pageProps }) {
-  return <div className="grain"><Component {...pageProps} /></div>
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>mwunh</title>
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="https://images.icon-icons.com/4046/PNG/512/youtube_logo_icon_256969.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
